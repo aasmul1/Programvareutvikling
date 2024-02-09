538e5c08346b98f7649ffb9f81305e7dbe3ce4ba
jakob <jakobhc@stud.ntnu.no>
2024-02-09 14:41:43 +0100

    Added /admin page for login No redirect upon succesfull login

--- explore/src/App.js
+++ explore/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './views/HomePage';
 import AdminConsole from './views/AdminConsole';
+import AdminLogin from './components/AdminLogin';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/adminconsole" element={<AdminConsole />} />
+        <Route path="/admin" element={<AdminLogin />} />
       </Routes>
     </Router>
   );
---
+++ explore/src/components/AdminLogin.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import './AdminLogin.css';
+import { auth } from '../firebase.js';
+
+const AdminLogin = ({ onLogin }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            // Send til adminconsole hvis login er sukseful
+            onLogin();
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+
+    return (
+        <div className="admin-login-container">
+            <h2>Login</h2>
+            {error && <p className="error-message">{error}</p>}
+            <form onSubmit={handleLogin}>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default AdminLogin;
