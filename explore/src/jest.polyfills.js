7b8091b8ab60dfca5d59d6fb6b6935bd5348e7d5
Oda <odaban@stud.ntnu.no>
2024-03-07 10:55:57 +0100

    Issue #8-a2 Add setImmediate polyfill
    Added for cross-environment compatibility.

--- explore/src/jest.polyfills.js
+++ explore/src/jest.polyfills.js
@@ -4,3 +4,13 @@ Object.defineProperties(globalThis, {
   TextDecoder: { value: TextDecoder },
   TextEncoder: { value: TextEncoder },
 })
+
+if (!window.setImmediate) {
+  window.setImmediate = function(callback) {
+    setTimeout(callback, 0);
+  };
+}
+
+setImmediate(function() {
+  console.log("This is a callback function.");
+});
