66b12cb11a6398a302769e3957109cac8230b96c
jakob <jakobhc@stud.ntnu.no>
2024-03-13 14:03:30 +0100

    Updated functional test for places.js

--- explore/src/views/__tests__/Places.test.js
+++ explore/src/views/__tests__/Places.test.js
@@ -1,58 +1,43 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import Places from '../Places';
-import { collection, getDocs, query } from 'firebase/firestore';
-import { db } from '../../firebase';
-
-jest.mock('@firebase/analytics', () => ({
-    getAnalytics: jest.fn(() => {}),
-}));
 
 jest.mock('firebase/firestore', () => ({
-    ...jest.requireActual('firebase/firestore'),
-    collection: jest.fn(),
-    getDocs: jest.fn(), 
+  ...jest.requireActual('firebase/firestore'),
+  collection: jest.fn(),
+  getDocs: jest.fn(),
+  query: jest.fn(),
+}));
+jest.mock('react-router-dom', () => ({
+  useNavigate: jest.fn(),
 }));
 
-test('fetches destinations from Firestore', async () => {
+describe('Places component', () => {
+  beforeEach(() => {
+    jest.clearAllMocks(); 
+  });
+
+  test('climate options should be in the document', () => {
+    render(<Places />);
+    const climateOption1 = screen.getByRole('option', { name: 'Climate' });
     
-    collection.mockReturnValueOnce(query);
-
+    expect(climateOption1).toBeInTheDocument();
    
+  });
+
+  test('type options should be in the document', () => {
     render(<Places />);
- 
-    expect(collection).toHaveBeenCalledWith(db, 'destinations');
-    expect(getDocs).toHaveBeenCalled(); 
+    const typeOption1 = screen.getByRole('option', { name: 'Type' });
+    
+    expect(typeOption1).toBeInTheDocument();
+   
+  });
+
+  test('search input should be in the document', () => {
+    render(<Places />);
+    const searchInput = screen.getByPlaceholderText('Search');
+    expect(searchInput).toBeInTheDocument();
+  });
+
 });
 
-test("renders vacation destinations heading", async () => {
-    render(<Places />);
-
-    const heading = screen.getByText("Vacation Destinations");
-    expect(heading).toBeInTheDocument();
-});
-
-test('fetches destinations from Firestore and displays them', async () => {
-    // Dummy
-    const dummyData = [
-        { id: '1', data: () => ({ destinationName: 'Destination 1', country: 'Country 1', destinationDescription: 'Description 1', url: 'https://example.com/image1.jpg' }) },
-        { id: '2', data: () => ({ destinationName: 'Destination 2', country: 'Country 2', destinationDescription: 'Description 2', url: 'https://example.com/image2.jpg' }) },
-    ];
-
-    collection.mockReturnValueOnce(query);
-    getDocs.mockResolvedValueOnce(dummyData);
-
-    render(<Places />);
-
-
-    await waitFor(() => {
-
-        const destination1 = screen.getByText('Destination 1');
-        const destination2 = screen.getByText('Destination 2');
-        
-        expect(destination1).toBeVisible();
-        expect(destination2).toBeVisible();
-    });
-
-    console.log('Destinations:', screen.getAllByText(/Destination \d/).map(element => element.textContent));
-});
