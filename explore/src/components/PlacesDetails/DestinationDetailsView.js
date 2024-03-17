bb5e48804e17851fdb4e72bf9f626d63ef20e4eb
olavhalleraker <ohallera@gmail.com>
2024-03-18 00:50:48 +0100

    #63-c9-weather-data:
    Added weather data to detailed destination view
    Changed layout on detailed destination view
    Minor styling changes in detailed dest. view.
    
    Co-authored-by: Camille Rød <camr@stud.ntnu.no>
    Co-authored-by: Eirill Bue <eirillbu@stud.ntnu.no>

--- explore/src/components/PlacesDetails/DestinationDetailsView.js
+++ explore/src/components/PlacesDetails/DestinationDetailsView.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import '../../styles/PlacesDetails/PlacesDetails.css'
+import WeatherData from './WeatherData';
 
 function DestinationDetailsView({ destination, currentImageUrl, onImageClick }) {
-    const formattedLongText = destination.LongText.split('\n').map((line, index) => (
-        <React.Fragment key={index}>
-          {line}
-          <br />
-        </React.Fragment>
-      ));
-      return (
-        <div>
-          <h1>{destination.destinationName}</h1>
-          <img src={currentImageUrl} alt={destination.destinationName} className="destination-image" />
-          <button onClick={onImageClick} className="view-more-images-btn">
-            View more images from {destination.destinationName}
-          </button>
-          <p className="destination-description">{destination.destinationDescription}</p>
-          <p className="long-text">{formattedLongText}</p>
+  const formattedLongText = destination.LongText.split('\n').map((line, index) => (
+    <React.Fragment key={index}>
+      {line}
+      <br />
+    </React.Fragment>
+  ));
+  return (
+    <body>
+      <div>
+        <h1>{destination.destinationName}</h1>
+      <div className='short-description'>
+        <p className="long-text">{destination.destinationDescription}</p>
+      </div>
+        <div className='destination-display'>
+          <WeatherData destination={destination} />
+          <div>
+            <img onClick={onImageClick} src={currentImageUrl} alt={destination.destinationName} className="destination-image" />
+          </div>
+
         </div>
-      );
+      <div className='long-description'>
+        <p className="long-text">{formattedLongText}</p>
+      </div>
+
+      </div>
+    </body>
+  );
 }
 
 export default DestinationDetailsView;
